8bafc52852795913887e6eb427f6712fc549414b
Luciano Bandeira <luciano062@gmail.com>
2018-11-07 13:02:51 -0300

    Convert to es6.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-'use strict';
+import request from 'sync-request';
+import url from 'parse';
+import isImage from 'is-image';
+import isUrl from 'is-url';
 
-const request = require('sync-request');
-const urlParse = require('url').parse;
-const isImage = require('is-image');
-const isUrl = require('is-url');
-
-module.exports = (url, accurate, timeout) => {
+export default (url, accurate, timeout) => {
   if (!url) return false;
   const http = url.lastIndexOf('http');
   if (http != -1) url = url.substring(http);
   if (!isUrl(url)) return isImage(url);
-  let pathname = urlParse(url).pathname;
+  let pathname = url.parse(url).pathname;
   if (!pathname) return false;
   const last = pathname.search(/[:?&]/);
   if (last != -1) pathname = pathname.substring(0, last);
